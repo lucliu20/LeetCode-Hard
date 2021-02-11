674d12376ee708d28a4dd0408f8a6574259201d9
lucliu20 <76449030+lucliu20@users.noreply.github.com>
2021-02-10 17:17:10 -0800

    Create 410-Split Array Largest Sum.py
    Place holder. Not resolved yet.

---
+++ 410-Split Array Largest Sum.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/split-array-largest-sum/
+
+"""
+Example 1:
+Input: nums = [7,2,5,10,8], m = 2
+Output: 18
+Explanation:
+There are four ways to split nums into two subarrays.
+The best way is to split it into [7,2,5] and [10,8],
+where the largest sum among the two subarrays is only 18.
+
+Example 2:
+Input: nums = [1,2,3,4,5], m = 2
+Output: 9
+
+Example 3:
+Input: nums = [1,4,4], m = 3
+Output: 4
+"""
+
+nums, m = [7,2,5,10,8], 2
+# nums, m = [1,2,3,4,5], 2
+# nums, m = [1,4,4], 3
+
+class Solution:
+    def splitArray(self, nums: list(), m: int) -> int:
+        pass
+
+solution = Solution()
+print(solution.splitArray(nums, m))
+
+# 
